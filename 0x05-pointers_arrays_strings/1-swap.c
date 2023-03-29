da7e84158cbbd088f8043eb5a17b249acf650f05
AnihNonso <anih.nonso@gmail.com>
2023-03-28 21:17:21 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swt_int - swap the value of 2 int
+ * @a: fint int
+ * @b: second int
+ * Return 0:
+ */
+
+void swap_int(int *a, int *b);
+{
+	int x;
+
+	s = *a;
+	*a = *b;
+	*b = s;
+}
